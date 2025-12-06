b6edd592ba0cfaa8cdd117d443438801da33791b
allenc42 <112156874+allenc42@users.noreply.github.com>
2025-12-05 18:28:53 -0800

    Changed logic

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -8,7 +8,7 @@ const GameOver = () => {
     const { pokemonGuess, setLivesCount, setPokemonGuess, pokemonInfo, setPokemonInfo} = useContext(PokemonContext);
 
     const navigate = useNavigate();
-    const [currentBest, setCurrentBest] = useState(0); 
+    const [currentBest, setCurrentBest] = useState(localStorage.getItem("best") || 0); 
 
 
     if(currentBest < pokemonGuess) {
